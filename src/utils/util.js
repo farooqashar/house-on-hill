4b9e1819ec29d4f7d87e10044a904ac60e4824d2
Ashar Farooq <ashar744@gmail.com>
2025-04-16 23:40:18 -0400

    merging basic intro and previous work

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,42 +1,70 @@
 export const allDots = [
-    {
-      id: "A",
-      speed: 0.05,
-      color: "#1f77b4",
-      description: "Whites",
-      anectode:
-        "No one thinks we can get evicted just because of how we look, but everyone's struggling nowadays.",
-      proportion_of_evictions: "10%",
-      eviction_trend: "white_group.PNG",
-    },
-    {
-      id: "B",
-      speed: 0.15,
-      color: "#ff7f0e",
-      description: "Blacks",
-      anectode:
-        "I remember going to the door and the sheriff standing there. It scared me because I didn't know why he was at my house.",
-      proportion_of_evictions: "24%",
-      eviction_trend: "black_group.PNG",
-    },
-    {
-      id: "C",
-      speed: 0.11,
-      color: "#2ca02c",
-      description: "Asians",
-      anectode:
-        "Everyone looks at us differently. In Boston, there are so many of us struggling due to insane costs.",
-      proportion_of_evictions: "12%",
-      eviction_trend: "asian_group.PNG",
-    },
-    {
-      id: "D",
-      speed: 0.14,
-      color: "#d62728",
-      description: "Latinos",
-      anectode:
-        "They have given me several notices that they are going to evict me, that they are going to take me to court, that they give me a few days.",
-      proportion_of_evictions: "17%",
-      eviction_trend: "hispanic_group.PNG",
-    },
-  ];
+  {
+    id: "A",
+    speed: 0.05,
+    color: "#1f77b4",
+    description: "White",
+    anectode:
+      "No one thinks we can get evicted just because of how we look, but everyone's struggling nowadays.",
+    // proportion_of_evictions: "10%",
+    eviction_trend: "white_group.PNG",
+    evictions_per_1000: "12.11",
+  },
+  {
+    id: "B",
+    speed: 0.15,
+    color: "#ff7f0e",
+    description: "Black",
+    anectode:
+      "I remember going to the door and the sheriff standing there. It scared me because I didn't know why he was at my house.",
+    // proportion_of_evictions: "24%",
+    eviction_trend: "black_group.PNG",
+    evictions_per_1000: "30.17",
+  },
+  {
+    id: "C",
+    speed: 0.11,
+    color: "#2ca02c",
+    description: "Asian",
+    anectode:
+      "Everyone looks at us differently. In Boston, there are so many of us struggling due to insane costs.",
+    // proportion_of_evictions: "12%",
+    eviction_trend: "asian_group.PNG",
+    evictions_per_1000: "16.11",
+  },
+  {
+    id: "D",
+    speed: 0.14,
+    color: "#d62728",
+    description: "Latinx",
+    anectode:
+      "They have given me several notices that they are going to evict me, that they are going to take me to court, that they give me a few days.",
+    // proportion_of_evictions: "17%",
+    eviction_trend: "hispanic_group.PNG",
+    evictions_per_1000: "23.08",
+  },
+  {
+    id: "E",
+    speed: 0.135,
+    color: "#9467bd",
+    description: "Native American",
+    anectode: "We’ve been priced out of our own neighborhoods for years.",
+    // proportion_of_evictions: "6%",
+    eviction_trend: "native_group.png",
+    avg_income: "$43,000",
+    housing_cost_burden: "29%",
+    evictions_per_1000: "25.79",
+  },
+  {
+    id: "F",
+    speed: 0.115,
+    color: "#8c564b",
+    description: "Pacific Islanders",
+    anectode: "Our story doesn't always get told, but we're struggling too.",
+    // proportion_of_evictions: "9%",
+    eviction_trend: "pacificislander_group.png",
+    avg_income: "$39,000",
+    housing_cost_burden: "30%",
+    evictions_per_1000: "18.81",
+  },
+];
